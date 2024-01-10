908911dbe7d0991c2177531b3d4c5c84aebe5378
Mark Antipin <markantipin@Marks-MacBook-Pro.local>
2024-01-10 12:54:57 +0100

    todos-v1

--- utils/make_request.py
+++ utils/make_request.py
@@ -4,9 +4,19 @@ from utils.logger import create_logger
 logger = create_logger()
 
 
-def make_request(url: str, **kwargs) -> httpx.Response:
+def make_request(
+    *,
+    url: str,
+    headers: dict | None = None,
+    params: dict | None = None,
+) -> httpx.Response | None:
+    response = httpx.get(
+        url,
+        headers=headers,
+        params=params
+    )
+
     try:
-        response = httpx.get(url, **kwargs)
         response.raise_for_status()
         return response
     except httpx.HTTPStatusError as exc:
