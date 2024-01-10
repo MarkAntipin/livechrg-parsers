c270fa6698d43e9c30fc1a3bd98e65597546188d
Andrey Rahimov <rahimov.work@gmail.com>
2024-01-10 11:36:31 +0600

    add function get_all_stations_list

--- src/parsers/charge_point/parser.py
+++ src/parsers/charge_point/parser.py
@@ -65,6 +65,45 @@ def get_stations_list(params=None) -> list | None:
     print(f'Stations_list has not been received from {url} because of error while requesting')
 
 
+def get_all_stations_list(
+        max_lat: int | float = 90,
+        min_lat: int | float = -90,
+        min_lon: int | float = -180,
+        max_lon: int | float = 180,
+        step: int | float = 10
+) -> list:
+    all_stations_list = []
+    lat = max_lat
+    lon = min_lon
+    while lat > min_lat:
+        while lon < max_lon:
+            params = dict(params_for_station_list_request)
+            ne_lat = lat
+            ne_lon = lon
+            sw_lat = max((lat - step), min_lat)
+            sw_lon =  min((lon + step), max_lon)
+            params['ne_lat'] = ne_lat
+            params['ne_lon'] = ne_lon
+            params['sw_lat'] = sw_lat
+            params['sw_lon'] = sw_lon
+            stations_list = get_stations_list(params=params)
+            if stations_list:
+                if len(stations_list) < 50:
+                    all_stations_list.extend(stations_list)
+                else:
+                    all_stations_list.extend(get_all_stations_list(
+                        max_lat=ne_lat,
+                        min_lat=sw_lat,
+                        min_lon=ne_lon,
+                        max_lon=sw_lon,
+                        step=step / 2
+                    ))
+            lon = lon + step
+        lat -= step
+        lon = min_lon
+    return all_stations_list
+
+
 def get_station_details(station_id: int) -> dict | None:
     response = _make_request(url=STATION_INFO_LINK_BASE, params={'deviceId': station_id})
     if response:
