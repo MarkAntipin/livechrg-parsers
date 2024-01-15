c643a4bf13341eb56efa49775f36ebf217ff1a12
Sergey Udachin <s.udachin@mail.ru>
2024-01-15 17:35:54 +0300

    feat: turn `create_logger` into `setup_logger`
    Took 19 minutes

--- utils/setup_logger.py
+++ utils/setup_logger.py
@@ -1,14 +1,10 @@
 import logging
 
 
-def create_logger(level=logging.DEBUG) -> logging.Logger:
+def setup_logger(logger: logging.Logger) -> None:
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(message)s')
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
 
-    logger = logging.getLogger(__name__)
-    logger.setLevel(level)
     logger.addHandler(console_handler)
-
-    return logger
