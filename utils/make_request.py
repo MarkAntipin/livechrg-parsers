243ea5eee48240fb2a4a71938a98f7f67e727d12
Andrey Rahimov <rahimov.work@gmail.com>
2024-01-18 11:50:39 +0600

    rename function mare_request to make_get_request; add make_post_request function

--- utils/make_request.py
+++ utils/make_request.py
@@ -7,26 +7,42 @@ logger = logging.getLogger(__name__)
 setup_logger(logger)
 
 
-def make_request(
+def make_get_request(
         *,
         url: str,
         headers: dict | None = None,
         params: dict | None = None,
-        method: str = 'get'
 ) -> httpx.Response | None:
-    methods: dict = {
-        'get': httpx.get,
-        'post': httpx.post,
-        'put': httpx.put,
-        'delete': httpx.delete,
-        'head': httpx.head,
-        'options': httpx.options,
-    }
-
-    response = methods[method](
-        url=url,
+    response = httpx.get(
+        url,
         headers=headers,
         params=params
+    )
+
+    try:
+        response.raise_for_status()
+        return response
+    except httpx.HTTPStatusError as exc:
+        logger.error(
+            "Status code %s: %s while requesting %s",
+            exc.response.status_code,
+            exc.response.text,
+            exc.request.url
+        )
+
+
+def make_post_request(
+        *,
+        url: str,
+        headers: dict | None = None,
+        params: dict | None = None,
+        json: dict | None = None,
+) -> httpx.Response | None:
+    response = httpx.post(
+        url,
+        headers=headers,
+        params=params,
+        json = json
     )
 
     try:
